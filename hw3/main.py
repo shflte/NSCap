6f8ff8a39570523c5e5ac3177ce0db668657f026
YC CHEN <shflte1314527@gmail.com>
2023-04-04 21:37:17 +0800

    update main

--- hw3/main.py
+++ hw3/main.py
@@ -1,43 +1,61 @@
 import protocols
 from setting import Setting
 import random
+import matplotlib.pyplot as plt
 
-def plt():
-    aloha_success=[]
-    slotted_aloha_success=[]
-    csma_success=[]
-    csma_cd_success=[]
-
-    aloha_success.append(success_rate)
-    slotted_aloha_success.append(success_rate)
-    csma_success.append(success_rate)
-    csma_cd_success.append(success_rate)
-
-    plt.plot(host_num_list, aloha_success, marker='o', label='ALOHA')
-    plt.plot(host_num_list, slotted_aloha_success, marker='s', label='Slotted ALOHA')
-    plt.plot(host_num_list, csma_success, marker='D', label='CSMA')
-    plt.plot(host_num_list, csma_cd_success, marker='*',label='CSMA/CD')
-    plt.xticks(host_num_list)
-    plt.xlabel('Number of Hosts')
-    plt.ylabel('Success Rate')
+def plotThing(title: str, xlabel: str, ylabel: str, alohaList: list(), slotted_alohaList: list(), csmaList: list(), csma_cdList: list(), xList: list()):
+    plt.title(title)
+    plt.plot(xList, alohaList, marker='o', label='ALOHA')
+    plt.plot(xList, slotted_alohaList, marker='s', label='Slotted ALOHA')
+    plt.plot(xList, csmaList, marker='D', label='CSMA')
+    plt.plot(xList, csma_cdList, marker='*',label='CSMA/CD')
+    plt.xticks(xList)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.legend()
     plt.show()
 
-# setting = Setting(host_num=3, total_time=100, packet_num=4, max_colision_wait_time=20, p_resend=0.3, packet_size=5, link_delay=1, seed=11234)
-setting = Setting(host_num=3, total_time=100, packet_num=4, max_colision_wait_time=20, p_resend=0.3, packet_size=3, link_delay=1, seed=4)
+aloha_success_rateList = []
+slotted_aloha_success_rateList = []
+csma_success_rateList = []
+csma_cd_success_rateList = []
 
-print("aloha")
-success_rate, idle_rate, collision_rate = protocols.aloha(setting, True)
-print(f'success_rate: {success_rate}\nidle_rate: {idle_rate}\ncollision_rate: {collision_rate}\n')
+aloha_idle_rateList = []
+slotted_aloha_idle_rateList = []
+csma_idle_rateList = []
+csma_cd_idle_rateList = []
 
-print("slotted_aloha")
-success_rate, idle_rate, collision_rate = protocols.slotted_aloha(setting, True)
-print(f'success_rate: {success_rate}\nidle_rate: {idle_rate}\ncollision_rate: {collision_rate}\n')
+aloha_collision_rateList = []
+slotted_aloha_collision_rateList = []
+csma_collision_rateList = []
+csma_cd_collision_rateList = []
 
-print("csma")
-success_rate, idle_rate, collision_rate = protocols.csma(setting, True)
-print(f'success_rate: {success_rate}\nidle_rate: {idle_rate}\ncollision_rate: {collision_rate}\n')
+host_num_list = [2,3,4,6]
+packet_num_list = [1200,800,600,400] # To ensure that the total number of packets remains constant.
+settings = [Setting(host_num=h, packet_num=p, max_colision_wait_time=20, p_resend=0.3) for h, p in zip(host_num_list, packet_num_list)]
 
-print("csma_cd")
-success_rate, idle_rate, collision_rate = protocols.csma_cd(setting, True)
-print(f'success_rate: {success_rate}\nidle_rate: {idle_rate}\ncollision_rate: {collision_rate}\n')
+for setting in settings:
+    aloha_success_rate, aloha_idle_rate, aloha_collision_rate = protocols.aloha(setting, False)
+
+    slotted_aloha_success_rate, slotted_aloha_idle_rate, slotted_aloha_collision_rate = protocols.slotted_aloha(setting, False)
+
+    csma_success_rate, csma_idle_rate, csma_collision_rate = protocols.csma(setting, False)
+
+    csma_cd_success_rate, csma_cd_idle_rate, csma_cd_collision_rate = protocols.csma_cd(setting, False)
+
+    aloha_success_rateList.append(aloha_success_rate)
+    slotted_aloha_success_rateList.append(slotted_aloha_success_rate)
+    csma_success_rateList.append(csma_success_rate)
+    csma_cd_success_rateList.append(csma_cd_success_rate)
+
+    aloha_idle_rateList.append(aloha_idle_rate)
+    slotted_aloha_idle_rateList.append(slotted_aloha_idle_rate)
+    csma_idle_rateList.append(csma_idle_rate)
+    csma_cd_idle_rateList.append(csma_cd_idle_rate)
+
+    aloha_collision_rateList.append(aloha_collision_rate)
+    slotted_aloha_collision_rateList.append(slotted_aloha_collision_rate)
+    csma_collision_rateList.append(csma_collision_rate)
+    csma_cd_collision_rateList.append(csma_cd_collision_rate)
+
+plotThing()
